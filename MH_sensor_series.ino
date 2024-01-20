858777d95b94a2fffdc6287129a15bf7ce7a95a0
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2024-01-20 16:44:06 -0500

    Start line follower code

--- MH_sensor_series.ino
+++ MH_sensor_series.ino
@@ -1,7 +1,8 @@
 const int IN_A0 = A0; // analog input
 
 void setup() {
-  pinMode (IN_A0, INPUT);
+  pinMode(IN_A0, INPUT);
+  Serial.begin(9600);
 }
 
 int value_A0;
@@ -9,11 +10,7 @@ int value_A0;
 void loop() {
   // A0 pin is the distance between the object and the obstacle
   value_A0 = analogRead(IN_A0); // reads the analog input from the IR distance sensor
-  
-  if (value_A0 > 900) {
-    Serial.println("BLACK DETECTED");
-  }
-  
-  delay(1000);
+  Serial.println(value_A0);
+  delay(100);
 }
 
